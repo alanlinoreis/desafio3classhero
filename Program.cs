83941d5751ecaa8e4be7bd3064da3c4ea1fabcc4
Alan Lino dos Reis <alaanlino@gmail.com>
2025-08-24 18:33:45 -0300

    Arquivos resolucao
    Resolucao exercicio

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] num = new int[7];
+Console.WriteLine("Digite 7 numeros: ");
+
+
+for (int i = 0; i < 7; i++)
+    num[i] = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 6; i >= 0; i--)
+    Console.WriteLine(num[i]);
